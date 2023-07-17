2b73038af0fe7f79f749d65b6c054640c126efef
zhangkai <zhangkai.gis@163.com>
2023-07-17 22:21:37 +0800

    define strategy floatup's struct

--- src/core/spot/strategies/seesaw_abs.rs
+++ src/core/spot/strategies/seesaw_abs.rs
@@ -1,7 +1,27 @@
 struct FloatUp {
+    src: token::Token,
+    dst: token::Token,
+    threshold: price::Price,
 }
 
 impl Strategy for FloatUp {
     fn execute(&self) {
+        assert!(self.src.price.symbol == self.dst.price.symbol);
+        assert!(self.src.price.symbol == self.threshold.symbol);
+
+    }
+}
+
+impl FloatUp{
+    pub fn set_src(&self, src token::Token){
+        self.src = src;
+    }
+    
+    pub fn set_dst(&self, dst token::Token){
+        self.dst = dst;
+    }
+
+    pub fun set_threshold(&self, threshold price::Price){
+        self.threshold = threshold;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ use base::token::price;
 
 fn main() {
     println!("Hello, world! {:?}", base::token::symbol::FIL);
-    let fil_price = price::Price::new(base::token::symbol::USDT, 4.1);
-    let dot_price = price::Price::new(base::token::symbol::USDT, 4.9);
-    let fil = token::Token::new(base::token::symbol::FIL, &fil_price);
+    let fil_price = price::Price::new(base::token::symbol::USDT.to_string(), 4.1);
+    let dot_price = price::Price::new(base::token::symbol::USDT.to_string(), 4.9);
+    let fil = token::Token::new(base::token::symbol::FIL.to_string(), &fil_price);
     println!("Hello, world! {:?}", fil);
-    let dot = token::Token::new(base::token::symbol::DOT, &dot_price);
-    let diff = dot.sub(&fil);
-    println!("Hello, world! {:?}", diff);
-    let therahold = price::Price::new(base::token::symbol::USDT, 0.4);
-    let mut stratage = core::spot::seesaw::SeesawAbs::new(&therahold);
-    stratage.set_source_coin(&fil);
-    stratage.set_target_coin(&dot);
-    println!("Hello, stratage  {:?}", stratage);
-    stratage.boom();
+//    let dot = token::Token::new(base::token::symbol::DOT.to_string(), &dot_price);
+//    let diff = dot.sub(&fil);
+//    println!("Hello, world! {:?}", diff);
+//    let therahold = price::Price::new(base::token::symbol::USDT.to_string(), 0.4);
+//    let mut stratage = core::spot::seesaw::SeesawAbs::new(&therahold);
+//    stratage.set_source_coin(&fil);
+//    stratage.set_target_coin(&dot);
+//    println!("Hello, stratage  {:?}", stratage);
+//    stratage.boom();
 }
